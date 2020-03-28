cbd3228ce3a86073592965b8e2fe0146b8c70ac7
Omar Bouzinab <omar@Omars-MacBook-Air.local>
2020-03-28 20:27:24 +0100

    useRef

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export default function App() {
   const [counter, setCounter] = useState(0);
   const [title, setTitle] = useState("");
 
-  const handleCounter = () => setCounter(c => c + 1);
+  const amountRef = useRef();
+  const reachMaxRef = useRef(false);
+
+  const handleCounter = () => {
+    if (!reachMaxRef.current) {
+      if (counter >= 10) reachMaxRef.current = true;
+      else setCounter(c => c + (parseInt(amountRef.current.value) || 1));
+    } else console.log("You reached max clicks!");
+  };
   const handleTitle = () => setTitle("Functionha academy");
 
   useEffect(() => {
@@ -13,15 +21,22 @@ export default function App() {
   }, [title]);
   useEffect(() => {
     console.log("Inside useEffect 2");
+    setTitle("");
     document.title = `You have clicked ${counter} times.`;
   }, [counter]);
 
+  useEffect(() => {
+    amountRef.current.focus();
+  });
+
   return (
     <div className="container text-center pt-5">
-      <button onClick={handleCounter} className="btn btn-primary">
+      <input ref={amountRef} />
+      <br />
+      <button onClick={handleCounter} className="btn btn-primary mt-3">
         Count up
       </button>
-      <button onClick={handleTitle} className="btn btn-primary">
+      <button onClick={handleTitle} className="btn btn-primary mt-3 ml-3">
         Change Title
       </button>
       <h1 className="mt-3">{counter}</h1>
