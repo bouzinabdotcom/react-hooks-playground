eba4ac647308e0752b84259b4d3e6ee3f16a5c81
Omar Bouzinab <omar@Omars-MacBook-Air.local>
2020-03-28 23:39:31 +0100

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import { Button } from "./Button";
 
 export default function App() {
   const [counter, setCounter] = useState(0);
   const [title, setTitle] = useState("");
 
   const amountRef = useRef();
-  const reachMaxRef = useRef(false);
+  // const reachMaxRef = useRef(false);
 
-  const handleCounter = () => {
-    if (!reachMaxRef.current) {
-      if (counter >= 10) reachMaxRef.current = true;
-      else setCounter(c => c + (parseInt(amountRef.current.value) || 1));
-    } else console.log("You reached max clicks!");
-  };
+  // const handleCounter = () => {
+  //   if (!reachMaxRef.current) {
+  //     if (counter >= 10) reachMaxRef.current = true;
+  //     else setCounter(c => c + (parseInt(amountRef.current.value) || 1));
+  //   } else console.log("You reached max clicks!");
+  // };
   const handleTitle = () => setTitle("Functionha academy");
 
   useEffect(() => {
-    console.log("Inside useEffect 1");
+    // console.log("Inside useEffect 1");
     document.title = title;
   }, [title]);
   useEffect(() => {
-    console.log("Inside useEffect 2");
+    // console.log("Inside useEffect 2");
     setTitle("");
     document.title = `You have clicked ${counter} times.`;
   }, [counter]);
@@ -29,13 +30,19 @@ export default function App() {
     amountRef.current.focus();
   });
 
+  const onClick = useCallback(n => setCounter(c => c + n), [setCounter]);
+
+  const listOfCount = [2, 4, 6, 10, 50];
+
   return (
     <div className="container text-center pt-5">
       <input ref={amountRef} />
       <br />
-      <button onClick={handleCounter} className="btn btn-primary mt-3">
-        Count up
-      </button>
+      <Button counterFn={onClick} label="Count Up" n={1} />
+      {listOfCount.map(count => (
+        <Button counterFn={onClick} label={count} key={count} n={count} />
+      ))}
+
       <button onClick={handleTitle} className="btn btn-primary mt-3 ml-3">
         Change Title
       </button>
