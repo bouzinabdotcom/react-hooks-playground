0b278de95bbf2a8d0151bd2e841d011505afb114
Omar Bouzinab <omar@Omars-MacBook-Air.local>
2020-03-28 19:36:42 +0100

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
   const [counter, setCounter] = useState(0);
+  const [title, setTitle] = useState("");
+
+  const handleCounter = () => setCounter(c => c + 1);
+  const handleTitle = () => setTitle("Functionha academy");
+
+  useEffect(() => {
+    console.log("Inside useEffect 1");
+    document.title = title;
+  }, [title]);
+  useEffect(() => {
+    console.log("Inside useEffect 2");
+    document.title = `You have clicked ${counter} times.`;
+  }, [counter]);
 
   return (
     <div className="container text-center pt-5">
-      <button
-        onClick={() => setCounter(c => c + 1)}
-        className="btn btn-primary"
-      >
+      <button onClick={handleCounter} className="btn btn-primary">
         Count up
+      </button>
+      <button onClick={handleTitle} className="btn btn-primary">
+        Change Title
       </button>
       <h1 className="mt-3">{counter}</h1>
     </div>
